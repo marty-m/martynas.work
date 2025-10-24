121023ec4d81d1592e8db4e7bab08070a9b8ea8d
marty-m <m.miliauskas2@ncl.ac.uk>
2025-10-24 14:06:55 +0200

    Configure AlbumNotFoundException

--- src/main/java/work/martynas/lens/album/errors/AlbumNotFoundException.java
+++ src/main/java/work/martynas/lens/album/errors/AlbumNotFoundException.java
@@ -1,4 +1,12 @@
 package work.martynas.lens.album.errors;
 
-public class AlbumNotFoundException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class AlbumNotFoundException extends RuntimeException {
+    public AlbumNotFoundException() {
+        super("Album not found");
+    }
+
 }
